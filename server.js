d9c4c4db5689ee13c2e6915d29511b77381c9094
Chakradhar Rao Jonagam <debianmaster@users.noreply.github.com>
2016-09-01 06:47:57 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,18 @@ const PORT = 8080;
 
 // App
 const app = express();
+
 app.get('/', function (req, res) {
   res.send('Hello world ' + os.hostname());
+});
+
+app.get('/healthz', function (req, res) {
+  res.send('OK');
+});
+
+app.get('/die', function (req, res) {
+  res.status(404)
+   .send('NOT OK');
 });
 
 app.listen(PORT);

8a9bd41df5946288018c9ea16458e1a555bdf13a
Chakradhar Rao Jonagam <debianmaster@users.noreply.github.com>
2016-10-05 09:38:34 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var os = require("os");
-// Constants
 const PORT = 8080;
-var healthy=false;
-// App
 const app = express();
+
+
+var healthy=true;
+
 
 app.get('/', function (req, res) {
   res.send('Hello world v1 ' + os.hostname() + '\n');
 });
+
+
 
 app.get('/healthz', function (req, res) {
   console.log('health enquiry')
@@ -18,10 +21,13 @@ app.get('/healthz', function (req, res) {
    res.status(404).send('NOT OK');
 });
 
+
 app.get('/cancer', function (req, res) {
    healthy=false;
    res.send('Killed ' + os.hostname());
 });
+
+
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);

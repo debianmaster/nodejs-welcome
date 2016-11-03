467a8d9f4c0201136ea52dd0ab5181aa8e07a3ed
Chakradhar Rao Jonagam <debianmaster@users.noreply.github.com>
2016-11-02 22:10:34 -0700

    Update server.js

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get('/dbtest',function(req,res){
      host     : process.env.mysql_host,
      user     : process.env.mysql_user,
      password : process.env.mysql_password,
-     database : process.env.database
+     database : process.env.mysql_database
    });
    connection.connect();
    connection.query('SELECT * from customer', function(err, rows, fields) {

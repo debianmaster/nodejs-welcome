581db6dd805f7f9c2caf9f0342278a3a376a154f
Chakradhar Rao Jonagam <debianmaster@users.noreply.github.com>
2019-06-01 10:03:42 +0800

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var healthy=true;
 
 
 app.get('/', function (req, res) {
-  res.send('Hello world v.1.26 ' + os.hostname() + '\n');
+  res.send('Hello world v.1.27 ' + os.hostname() + '\n');
 });
 
 
